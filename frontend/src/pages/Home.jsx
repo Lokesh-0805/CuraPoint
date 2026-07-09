e9a8e38c7f2a46114d5c5997d0fecd5314181ea0
Lokesh Chahar <lokeshchahar802@gmail.com>
2026-07-09 16:08:17 +0530

    more components and context added.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Header from '../components/Header'
+import SpecialityMenu from '../components/SpecialityMenu'
+import TopDotors from '../components/TopDotors'
+import Banner from '../components/Banner'
 
 const Home = () => {
   return (
     <div>
         <Header/>
+        <SpecialityMenu/>
+        <TopDotors/>
+        <Banner/>
     </div>
   )
 }
