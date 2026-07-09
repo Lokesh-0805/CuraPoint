1f545c22d71f7e056914b93f734d1752a54fd89c
Lokesh Chahar <lokeshchahar802@gmail.com>
2026-07-10 00:46:56 +0530

    Appointment page.

---
+++ frontend/src/components/RelatedDoctors.jsx
@@ -0,0 +1,16 @@
+import React,{useContext} from 'react'
+import { AppContext } from '../context/AppContext'
+
+const RelatedDoctors = ({speciality,docId}) => {
+    const {doctors} = useContext(AppContext)
+
+    const [relDoc,setRelDocs] = useState([])
+
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default RelatedDoctors
