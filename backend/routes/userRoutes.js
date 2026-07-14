19e3b22dbad8af550b764a1b534bde36f201cb25
Lokesh Chahar <lokeshchahar802@gmail.com>
2026-07-14 19:36:05 +0530

    Appointment cancel added and Razorpay integrated.

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { registerUser,loginUser, getProfile, updateProfile,bookAppointment, listAppointment } from '../controllers/userController.js'
+import { registerUser,loginUser, getProfile, updateProfile,bookAppointment, listAppointment, cancelAppointment, paymentRazorpay, verifyRazorpay } from '../controllers/userController.js'
 import authUser from '../middlewares/authUser.js'
 import upload from '../middlewares/multer.js'
 import authUserget from '../middlewares/authUserget.js'
@@ -13,6 +13,10 @@ userRouter.get('/get-profile',authUserget,getProfile)
 userRouter.post('/update-profile',upload.single('image'),authUser,updateProfile)
 userRouter.post('/book-appointment',authUser,bookAppointment)
 userRouter.get('/appointments',authUserget,listAppointment)
+userRouter.post('/cancel-appointment',authUser,cancelAppointment)
+userRouter.post('/payment-razorpay',authUser,paymentRazorpay)
+userRouter.post('/verify-razorpay',authUser,verifyRazorpay)
+
 
 
 export default userRouter
