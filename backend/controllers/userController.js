30a7378a0fd223047330a1ec65da211a3b16b9ec
Lokesh Chahar <lokeshchahar802@gmail.com>
2026-07-14 16:39:32 +0530

    Appointment page working.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,8 @@
 import validator from 'validator'
 import bcrypt from 'bcrypt'
 import userModel from '../models/userModel.js'
+import doctorModel from '../models/doctorModel.js'
+import appointmentModel from '../models/appointmentModel.js'
 import jwt from 'jsonwebtoken'
 import {v2 as cloudinary} from 'cloudinary'
 import upload from '../middlewares/multer.js'
